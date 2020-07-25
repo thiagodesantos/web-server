91570ff8ec3861a1015949f8084d7e14d915161e
thiagodesantos <39632450+thiagodesantos@users.noreply.github.com>
2020-07-25 10:54:51 -0300

    adicionado temperatura maxima e minima

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,14 @@ const forecast = (latitude, longitude, callback) =>{
            callback('Não foi possivel connectar ao serviço de coordenada!', undefined)
         } else if(body.error){
            callback('Não foi possivel encontrar essa coordenada, tente outra', undefined)
-        } else{        
-            callback(undefined, body.daily.data[0].summary + ' Está atualmente ' + body.currently.temperature + ' graus. Tem uma chance de ' + body.currently.precipProbability + '% de chuva.')           
+        } else{                 
+            callback(
+               undefined, 
+               body.daily.data[0].summary + ' Está atualmente ' + 
+               body.currently.temperature + ' graus. A máxima hoje é de '+ 
+               body.daily.data[0].temperatureHigh + ' com a mínima de ' + 
+               body.daily.data[0].temperatureLow +'. Tem uma chance de ' + 
+               body.currently.precipProbability + '% de chuva.')           
         }
      })
 }
